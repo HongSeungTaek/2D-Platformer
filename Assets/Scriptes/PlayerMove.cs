1d17b46f93123c8c6ae55051f7b19469f82819e8
홍승택 <tmdxor66@naver.com>
2021-06-17 23:04:00 +0900

    피격 후 애니메이션 효과 추가

--- Assets/Scriptes/PlayerMove.cs
+++ Assets/Scriptes/PlayerMove.cs
@@ -69,7 +69,31 @@ public class PlayerMove : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy") {
-            Debug.Log("플레이어가 맞았습니다!");
+            OnDamaged(collision.transform.position);
         }
+    }
+
+    void OnDamaged(Vector2 targetPos)
+    {
+        // Change Layer (Immortal Active)
+        gameObject.layer = 9;
+
+        // View Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // Reaction Force
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc,1)*7, ForceMode2D.Impulse);
+
+        // Animation
+        anim.SetTrigger("doDamaged");
+
+        Invoke("OffDamaged", 3);
+    }
+
+    void OffDamaged()
+    {
+        gameObject.layer = 8;
+        spriteRenderer.color = new Color(1, 1, 1, 1);
     }
 }
